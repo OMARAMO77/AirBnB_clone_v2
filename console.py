daf102923a3e8ff9fc85e5312fe445bf404dac42
Omar <omar0642624692@gmail.com>
2023-09-19 04:12:41 -0700

    console improved

--- console.py
+++ console.py
@@ -115,16 +115,42 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, args):
         """ Create an object of any class"""
-        if not args:
+        arg = args.split()
+        if len(arg) == 0:
             print("** class name missing **")
             return
-        elif args not in HBNBCommand.classes:
+        elif arg[0] not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
-        new_instance = HBNBCommand.classes[args]()
-        storage.save()
-        print(new_instance.id)
-        storage.save()
+        else:
+            cls_name = arg[0]
+            if len(args) > 1:
+                list_param = arg[1:]
+            param = {}
+            for item in list_param:
+                key_value = item.split("=")
+                if len(key_value) != 2:
+                    continue
+                key = key_value[0]
+                value = key_value[1]
+                if value[0] == '"' and value[-1] == '"':
+                    value = value.replace("_", " ")
+                    value = value.strip('\"')
+                elif "." in value:
+                    try:
+                        value = float(value)
+                    except ValueError:
+                        continue
+                elif value.isnumeric():
+                    value = int(value)
+                else:
+                    continue
+                param[key] = value
+            new_instance = HBNBCommand.classes[cls_name]()
+            for key, value in param.items():
+                setattr(new_instance, key, value)
+            new_instance.save()
+            print(new_instance.id)
 
     def help_create(self):
         """ Help information for the create method """
