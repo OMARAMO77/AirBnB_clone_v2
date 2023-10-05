108ad523cbecf923e9c99239c7f3846c0276a507
Omar <omar0642624692@gmail.com>
2023-10-05 15:49:14 -0700

    101-setup_web_static.pp

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,69 +1,124 @@
-include stdlib
+#!/usr/bin/puppet apply
+# Script that redoes task 0 (sets up your web servers for the deployment of web_static)
 
-# Update the package lists
-exec { 'Update lists':
-    command => '/usr/bin/apt update'
+exec { 'apt-get-update':
+  command => '/usr/bin/apt-get update',
+  path    => '/usr/bin:/usr/sbin:/bin',
 }
 
-# Install the web serverNginx
+exec { 'remove-current':
+  command => 'rm -rf /data/web_static/current',
+  path    => '/usr/bin:/usr/sbin:/bin',
+}
+
 package { 'nginx':
-    ensure  => 'present',
-    require => Exec['Update lists']
+  ensure  => installed,
+  require => Exec['apt-get-update'],
 }
 
-# Create the directory tree
-exec { 'Create Directory Tree':
-    command => '/bin/mkdir -p /data/web_static/releases/test /data/web_static/shared',
-    require => Package['nginx']
+file { '/var/www':
+  ensure  => directory,
+  mode    => '0755',
+  recurse => true,
+  require => Package['nginx'],
 }
 
-$head = "  <head>\n  </head>"
-$body = "  <body>\n    Holberton School\n  </body>"
-$index = "<html>\n${head}\n${body}\n</html>\n"
-
-# Create a fake HTML file with some content,
-# to test Nginx configuration
-file { 'Create Fake HTML':
-    ensure  => 'present',
-    path    => '/data/web_static/releases/test/index.html',
-    content => $index,
-    require => Exec['Create Directory Tree']
+file { '/var/www/html/index.html':
+  content => 'Hello, World!',
+  require => File['/var/www'],
 }
 
-# Create a symbolic link '/data/web_static/current' linked to the
-# '/data/web_static/releases/test/' folder.
-file { 'Create Symbolic Link':
-    ensure  => 'link',
-    path    => '/data/web_static/current',
-    force   => true,
-    target  => '/data/web_static/releases/test',
-    require => File['Create Fake HTML']
+file { '/var/www/error/404.html':
+  content => "Ceci n'est pas une page",
+  require => File['/var/www'],
 }
 
-# Ensures that the web server Nginx is running
-service { 'nginx':
-    ensure  => 'running',
-    enable  => true,
-    require => Package['nginx']
+exec { 'make-static-files-folder':
+  command => 'mkdir -p /data/web_static/releases/test /data/web_static/shared',
+  path    => '/usr/bin:/usr/sbin:/bin',
+  require => Package['nginx'],
 }
 
-# Set permissions for the 'ubuntu' user
-exec { 'Set permissions':
-    command => '/bin/chown -R ubuntu:ubuntu /data',
-    require => File['Create Symbolic Link']
+file { '/data/web_static/releases/test/index.html':
+  content =>
+"<!DOCTYPE html>
+<html lang='en-US'>
+	<head>
+		<title>Home - AirBnB Clone</title>
+	</head>
+	<body>
+		<h1>Welcome to AirBnB!</h1>
+	<body>
+</html>
+",
+  replace => true,
+  require => Exec['make-static-files-folder'],
 }
 
-# Set a new location for a Nginx VHost 
-$loc_header='location /hbnb_static/ {'
-$loc_content='alias /data/web_static/current/;'
-$new_location="\n\t${loc_header}\n\t\t${loc_content}\n\t}\n"
-
-# Write the new location to the default Nginx VHost
-file_line { 'Set Nginx Location':
-    ensure  => 'present',
-    path    => '/etc/nginx/sites-available/default',
-    after   => 'server_name \_;',
-    line    => $new_location,
-    notify  => Service['nginx'],
-    require => Exec['Set permissions']
+exec { 'link-static-files':
+  command => 'ln -sf /data/web_static/releases/test/ /data/web_static/current',
+  path    => '/usr/bin:/usr/sbin:/bin',
+  require => [
+    Exec['remove-current'],
+    File['/data/web_static/releases/test/index.html'],
+  ],
 }
+
+exec { 'change-data-owner':
+  command => 'chown -hR ubuntu:ubuntu /data',
+  path    => '/usr/bin:/usr/sbin:/bin',
+  require => Exec['link-static-files'],
+}
+
+file { '/etc/nginx/sites-available/default':
+  ensure  => present,
+  mode    => '0644',
+  content =>
+"server {
+	listen 80 default_server;
+	listen [::]:80 default_server;
+	server_name _;
+	index index.html index.htm;
+	error_page 404 /404.html;
+	add_header X-Served-By \$hostname;
+	location / {
+		root /var/www/html/;
+		try_files \$uri \$uri/ =404;
+	}
+	location /hbnb_static/ {
+		alias /data/web_static/current/;
+		try_files \$uri \$uri/ =404;
+	}
+	if (\$request_filename ~ redirect_me){
+		rewrite ^ https://sketchfab.com/bluepeno/models permanent;
+	}
+	location = /404.html {
+		root /var/www/error/;
+		internal;
+	}
+}",
+  require => [
+    Package['nginx'],
+    File['/var/www/html/index.html'],
+    File['/var/www/error/404.html'],
+    Exec['change-data-owner']
+  ],
+}
+
+exec { 'enable-site':
+  command => "ln -sf '/etc/nginx/sites-available/default' '/etc/nginx/sites-enabled/default'",
+  path    => '/usr/bin:/usr/sbin:/bin',
+  require => File['/etc/nginx/sites-available/default'],
+}
+
+exec { 'start-nginx':
+  command => 'sudo service nginx restart',
+  path    => '/usr/bin:/usr/sbin:/bin',
+  require => [
+    Exec['enable-site'],
+    Package['nginx'],
+    File['/data/web_static/releases/test/index.html'],
+  ],
+}
+
+Exec['start-nginx']
