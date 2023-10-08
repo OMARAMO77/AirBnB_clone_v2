791c0339d0eebf140c671a15d0fa695b34f4c4a9
Omar <omar0642624692@gmail.com>
2023-10-08 16:46:44 -0700

    101-setup_web_static.pp

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,77 +1,69 @@
-# Ensure Nginx is installed
+include stdlib
+
+# Update the package lists
+exec { 'Update lists':
+    command => '/usr/bin/apt update'
+}
+
+# Install the web serverNginx
 package { 'nginx':
-  ensure => installed,
+    ensure  => 'present',
+    require => Exec['Update lists']
 }
 
-# Create necessary directories
-file { '/data':
-  ensure  => directory,
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
-  recurse => true,
+# Create the directory tree
+exec { 'Create Directory Tree':
+    command => '/bin/mkdir -p /data/web_static/releases/test /data/web_static/shared',
+    require => Package['nginx']
 }
 
-file { '/data/web_static':
-  ensure  => directory,
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
+$head = "  <head>\n  </head>"
+$body = "  <body>\n    Holberton School\n  </body>"
+$index = "<html>\n${head}\n${body}\n</html>\n"
+
+# Create a fake HTML file with some content,
+# to test Nginx configuration
+file { 'Create Fake HTML':
+    ensure  => 'present',
+    path    => '/data/web_static/releases/test/index.html',
+    content => $index,
+    require => Exec['Create Directory Tree']
 }
 
-file { '/data/web_static/releases/test':
-  ensure  => directory,
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
+# Create a symbolic link '/data/web_static/current' linked to the
+# '/data/web_static/releases/test/' folder.
+file { 'Create Symbolic Link':
+    ensure  => 'link',
+    path    => '/data/web_static/current',
+    force   => true,
+    target  => '/data/web_static/releases/test',
+    require => File['Create Fake HTML']
 }
 
-file { '/data/web_static/shared':
-  ensure  => directory,
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
-}
-
-# Create index.html file
-file { '/data/web_static/releases/test/index.html':
-  ensure  => file,
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
-  content => '<html>
-                <head>
-                </head>
-                <body>
-                  Holberton School
-                </body>
-              </html>',
-}
-
-# Create symbolic link
-file { '/data/web_static/current':
-  ensure => link,
-  target => '/data/web_static/releases/test',
-  force  => true,
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
-}
-
-# Update Nginx configuration
-file { '/etc/nginx/sites-available/default':
-  ensure  => present,
-  content => "server {
-                listen 80 default_server;
-                listen [::]:80 default_server;
-
-                server_name _;
-
-                location /hbnb_static {
-                    alias /data/web_static/current;
-                }
-                # Other configurations...
-            }",
-  notify  => Service['nginx'],
-}
-
-# Restart Nginx
+# Ensures that the web server Nginx is running
 service { 'nginx':
-  ensure    => running,
-  enable    => true,
-  subscribe => File['/etc/nginx/sites-available/default'],
+    ensure  => 'running',
+    enable  => true,
+    require => Package['nginx']
+}
+
+# Set permissions for the 'ubuntu' user
+exec { 'Set permissions':
+    command => '/bin/chown -R ubuntu:ubuntu /data',
+    require => File['Create Symbolic Link']
+}
+
+# Set a new location for a Nginx VHost 
+$loc_header='location /hbnb_static/ {'
+$loc_content='alias /data/web_static/current/;'
+$new_location="\n\t${loc_header}\n\t\t${loc_content}\n\t}\n"
+
+# Write the new location to the default Nginx VHost
+file_line { 'Set Nginx Location':
+    ensure  => 'present',
+    path    => '/etc/nginx/sites-available/default',
+    after   => 'server_name \_;',
+    line    => $new_location,
+    notify  => Service['nginx'],
+    require => Exec['Set permissions']
 }
