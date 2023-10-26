80faf82e90b71b7ef060d53baa88abb8fc6b86bd
Omar <omar0642624692@gmail.com>
2023-10-26 12:11:02 -0700

    7-states_list.py

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+"""Starts a Flask web application"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+import models
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Displays 'Hello HBNB!'"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Displays 'HBNB'"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """Replace underscores with spaces in text"""
+    text = text.replace('_', ' ')
+    return "C {}".format(text)
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text='is_cool'):
+    """Replace underscores with spaces in text"""
+    text = text.replace('_', ' ')
+    return "Python {}".format(text)
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """Displays 'n is a number' if n is an integer"""
+    return "{} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def num_template(n=None):
+    """Displays a HTML page only if n is an integer"""
+    return render_template('5-number.html', number=n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def odd_or_even(n):
+    """Displays a HTML page only if n is an integer"""
+    parity = 'even' if n % 2 == 0 else 'odd'
+    return render_template('6-number_odd_or_even.html', n=n, parity=parity)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """Display a HTML page of the States"""
+    states = storage.all(State)
+    return render_template('7-states_list.html', states=states)
+
+@app.teardown_appcontext
+def teardown_db(error):
+    """Closes the database again at the end of the request."""
+    storage.close()
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000)
