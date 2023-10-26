32a887c1da86a0322f90b337adb30b3fba7072a0
Omar <omar0642624692@gmail.com>
2023-10-26 12:14:50 -0700

    7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -60,10 +60,12 @@ def states_list():
     states = storage.all(State)
     return render_template('7-states_list.html', states=states)
 
+
 @app.teardown_appcontext
 def teardown_db(error):
     """Closes the database again at the end of the request."""
     storage.close()
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000)
